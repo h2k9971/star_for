e10c3c845b24648b6c130544d4f14a5da8b4de13
hyunho kim <h2k9971@naver.com>
2022-07-07 17:19:55 +0900

    버전명: [V6] 왼쪽 역삼각형 for문으로 구현
    버전 세부내용: 왼쪽 역삼각형을 for문 함수로 구현

--- star3.py
+++ star3.py
@@ -1,7 +1,5 @@
-# star3_print.py
-
-print("*****")
-print(" ****")
-print("  ***")  
-print("   **") 
-print("    *") 
+# for문 이용 구현
+  
+  N=6
+  for i in range(1, N):
+     print(" " *i + "*" * (N-i))
